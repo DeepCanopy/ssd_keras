70256a2508da7827de7dc0a9ef6efea81fd58113
hobs <hobs+github@totalgood.com>
2019-01-26 06:52:51 -0800

    keras 2

--- mnist.py
+++ mnist.py
@@ -50,7 +50,7 @@ model = Sequential()
 model.add(Conv2D(32, (3, 3), activation="relu", input_shape=(1, 28, 28)))
 print(model.output_shape)
 # (None, 32, 26, 26)
-model.add(Conv2D(32, (3, 3), activation="relu")
+model.add(Conv2D(32, (3, 3), activation="relu"))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
  
