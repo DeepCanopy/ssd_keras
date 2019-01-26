46ea6202cbf3eba18a7214ec1a980f4719176048
hobs <hobs+github@totalgood.com>
2019-01-26 06:48:14 -0800

    print for py3

--- mnist.py
+++ mnist.py
@@ -1,18 +1,27 @@
+import numpy as np
+np.random.seed(123)  # for reproducibility
+from keras.models import Sequential
+
 from keras.datasets import mnist
- 
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+
+
 # Load pre-shuffled MNIST data into train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-print X_train.shape
+print(X_train.shape)
 # (60000, 28, 28)
 
 from matplotlib import pyplot as plt
 plt.imshow(X_train[0])
 
+
 X_train = X_train.reshape(X_train.shape[0], 1, 28, 28)
 X_test = X_test.reshape(X_test.shape[0], 1, 28, 28)
 
-print X_train.shape
+print(X_train.shape)
 # (60000, 1, 28, 28)
 
 X_train = X_train.astype('float32')
@@ -20,10 +29,10 @@ X_test = X_test.astype('float32')
 X_train /= 255
 X_test /= 255
 
-print y_train.shape
+print(y_train.shape)
 # (60000,)
 
-print y_train[:10]
+print(y_train[:10])
 # [5 0 4 1 9 2 1 3 1 4]
 
 
@@ -31,7 +40,7 @@ print y_train[:10]
 Y_train = np_utils.to_categorical(y_train, 10)
 Y_test = np_utils.to_categorical(y_test, 10)
 
-print Y_train.shape
+print(Y_train.shape)
 # (60000, 10)
 
 
@@ -39,7 +48,7 @@ print Y_train.shape
 model = Sequential()
  
 model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(1,28,28)))
-print model.output_shape
+print(model.output_shape)
 # (None, 32, 26, 26)
 model.add(Convolution2D(32, 3, 3, activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
