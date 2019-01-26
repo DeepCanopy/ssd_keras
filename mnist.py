f91b2b984db23c3c6d05902e22242a0bae1a3dff
hobs <hobs+github@totalgood.com>
2019-01-26 06:53:27 -0800

    keras 2

--- mnist.py
+++ mnist.py
@@ -4,7 +4,7 @@ from keras.models import Sequential
 
 from keras.datasets import mnist
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 
 
