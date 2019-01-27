5c34e9f8dcc4ac1ccc905c5a30ccc47bd081baa1
hobs <hobs+github@totalgood.com>
2019-01-26 18:00:06 -0800

    missing commas

--- ssd300_training.py
+++ ssd300_training.py
@@ -203,13 +203,11 @@ val_dataset = DataGenerator(load_images_into_memory=False, hdf5_dataset_path=Non
 # 2: Parse the image and label lists for the training and validation datasets. This can take a while.
 
 # TODO: Set the paths to the datasets here.
-VOC_2007_DIR = os.path.join('/', 'midata', 'VOC2007', 'VOCdevkit', 'VOC2007')
-VOC_2012_DIR = os.path.join('/', 'midata', 'VOC2012', 'VOCdevkit', 'VOC2012')
-# The directories that contain the images.
+VOCDEVKIT_DIR = os.path.join('/', 'midata', 'PASCAL_VOC', 'VOCdevkit')
+VOC_2007_DIR = os.path.join(VOCDEVKIT_DIR, 'VOC2007')
+VOC_2012_DIR = os.path.join(VOCDEVKIT_DIR, 'VOC2012')
 VOC_2007_images_dir      = os.path.join(VOC_2007_DIR, 'JPEGImages')
 VOC_2012_images_dir      = os.path.join(VOC_2012_DIR, 'JPEGImages')
-
-# The directories that contain the annotations.
 VOC_2007_annotations_dir      = os.path.join(VOC_2007_DIR, 'Annotations')
 VOC_2012_annotations_dir      = os.path.join(VOC_2012_DIR, 'Annotations')
 
